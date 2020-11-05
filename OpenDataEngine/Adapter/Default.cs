a76735ad8511bb9e6438406ffcb04d0ef6dacd58
Chris Kruining <c.kruining@fyn.nl>
2020-11-05 18:00:25 +0100

    added ability for expression visitor to recognize if the member expression is actually correct

---
+++ OpenDataEngine/Adapter/Default.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenDataEngine.Adapter
+{
+    public class Default : Base
+    {
+        public override (String Command, (String, Object)[] Arguments) Translate(IAsyncQueryable query)
+        {
+            return default;
+        }
+
+        public override async IAsyncEnumerable<TResult> From<TResult>(IAsyncEnumerable<IDictionary<String, dynamic>> source, CancellationToken token)
+        {
+            await Task.CompletedTask;
+
+            yield break;
+        }
+    }
+}
---
+++ OpenDataEngine/Connection/Default.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OpenDataEngine.Connection
+{
+    public class Default : Base
+    {
+        public override Task Connect(CancellationToken token)
+        {
+            return Task.CompletedTask;
+        }
+
+        public override async IAsyncEnumerable<IDictionary<String, dynamic>> Execute(String command, (String, Object)[] arguments, CancellationToken token)
+        {
+            await Task.CompletedTask;
+
+            yield break;
+        }
+    }
+}
